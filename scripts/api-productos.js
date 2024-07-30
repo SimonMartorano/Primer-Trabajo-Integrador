a2f514a55ab0e25bb21279192ae4cacb9ef46d43
simon <simonmartorano@gmail.com>
2024-07-30 02:27:13 -0300

    Se creo el archivo api-productos.js desde donde se cargaran los productos, tambien se modifico script.js para que solo se carguen max 4 productos en la pagina de inicio

---
+++ scripts/api-productos.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        nombre: "Coca Cola 2,25Lts.",
+        precio: 3100,
+        imagen: "/design/images/coca-cola-2.25lts.webp",
+        alt: "Coca Cola 2,25 litros",
+        descuento: 50,
+        stock: 200,
+    },
+    
+    {
+        nombre: "Cerveza Imperial Stout lata 473 Ml.",
+        precio: 1690,
+        imagen: "/design/images/cerveza-imperial-stout-473ml.webp",
+        alt: "Cerveza Imperial stout 473 mililitros",
+        descuento: 0,
+        stock: 100
+    },
+
+    {
+        nombre: "Fideos tirabuzón Matarazzo 500g",
+        precio: 1400,
+        imagen: "/design/images/fideos-tirabuzon-matarazzo-500g.webp",
+        alt: "Fideos tirabuzon Matarazzo 500 gramos",
+        descuento: 0,
+        stock: 70
+    },
+
+    {
+        nombre: "Leche Larga Vida Liviana La Serenisima 1L",
+        precio: 1840,
+        imagen: "/design/images/leche-liviana-laserenisima-1L.webp",
+        alt: "Leche larga vida liviana La Serenisima 1 litro",
+        descuento: 10,
+        stock: 80
+    },
+
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,56 +1,23 @@
 "use strict";
 
-const productos = [
-    {
-        nombre: "Coca Cola 2,25Lts.",
-        precio: 3100,
-        imagen: "/design/images/coca-cola-2.25lts.webp",
-        alt: "Coca Cola 2,25 litros",
-        descuento: 50
-    },
-    
-    {
-        nombre: "Cerveza Imperial Stout lata 473 Ml.",
-        precio: 1690,
-        imagen: "/design/images/cerveza-imperial-stout-473ml.webp",
-        alt: "Cerveza Imperial stout 473 mililitros",
-        descuento: 30
-    },
-
-    {
-        nombre: "Fideos tirabuzón Matarazzo 500g",
-        precio: 1400,
-        imagen: "/design/images/fideos-tirabuzon-matarazzo-500g.webp",
-        alt: "Fideos tirabuzon Matarazzo 500 gramos",
-        descuento: 20
-    },
-
-    {
-        nombre: "Leche Larga Vida Liviana La Serenisima 1L",
-        precio: 1840,
-        imagen: "/design/images/leche-liviana-laserenisima-1L.webp",
-        alt: "Leche larga vida liviana La Serenisima 1 litro",
-        descuento: 10
-    },
-
-];
 const $contenedorProductos = document.querySelector(".contenedor-productos");
 const $template = document.querySelector("template").content;
 const $fragment = document.createDocumentFragment();
 
-productos.forEach((producto) => {
+for(let i = 0; i < 4; i++){
 
-    let precioFinal = producto.precio - (producto.precio * producto.descuento / 100) 
+    let precioFinal = productos[i].precio - (productos[i].precio * productos[i].descuento / 100) 
 
-    $template.querySelector("img").setAttribute("src", producto.imagen);
-    $template.querySelector("img").setAttribute("alt", producto.alt);
-    $template.querySelector("h2").textContent = producto.nombre;
-    $template.querySelector(".precio-base").textContent = "$ " + producto.precio;
-    $template.querySelector(".descuento").textContent = `-${producto.descuento}%`;
+    $template.querySelector("img").setAttribute("src", productos[i].imagen);
+    $template.querySelector("img").setAttribute("alt", productos[i].alt);
+    $template.querySelector("h2").textContent = productos[i].nombre;
+    $template.querySelector(".precio-base").textContent = "$ " + productos[i].precio;
+    $template.querySelector(".descuento").textContent = `-${productos[i].descuento}%`;
     $template.querySelector(".precio-final").textContent = "$ " + precioFinal;
 
     let $clon = document.importNode($template, true);
     $fragment.appendChild($clon);
-});
+
+}
 
 $contenedorProductos.appendChild($fragment);
